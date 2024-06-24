0d9f9106ce052f8d119efb87d3753ff3589dfcc0
Matthew Lilley <hello@matthewlilley.com>
2024-06-24 11:08:59 +0100

    chore: update api example

--- route-processor/api.ts
+++ route-processor/api.ts
@@ -9,10 +9,15 @@ const publicClient = createPublicClient({
 })
 
 // https://production.sushi.com/swap/v3.2?chainId=1&tokenIn=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&tokenOut=0x6B3595068778DD592e39A122f4f5a5cF09C90fE2&amount=1000000000000000&maxPriceImpact=0.005&gasPrice=89998588640&to=0x23DefC2Ca207e7FBD84AE43B00048Fb5Cb4DB5B2&preferSushi=true
-const SWAP_API_URL = new URL('https://production.sushi.com/swap/v3.2')
+
+// https://api.sushi.com/swap/v4/1?tokenIn=0x1590ABe3612Be1CB3ab5B0f28874D521576e97Dc&tokenOut=0x0D8775F648430679A709E98d2b0Cb6250d2887EF&amount=6725081405432150000&maxPriceImpact=0.005&gasPrice=34398060102&to=0x8f54C8c2df62c94772ac14CcFc85603742976312&preferSushi=true
 
 // Chain (Ethereum)
 const chainId = 1
+
+const SWAP_API_URL = new URL('https://api.sushi.com/swap/v4/' + chainId)
+
+
 
 // TokenA & TokenB
 const inputCurrency = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
@@ -31,8 +36,6 @@ const gasPrice = await publicClient.getGasPrice()
 const to = '0x8f54C8c2df62c94772ac14CcFc85603742976312'
 
 const { searchParams } = SWAP_API_URL
-
-searchParams.set('chainId', chainId.toString())
 searchParams.set('tokenIn', inputCurrency)
 searchParams.set('tokenOut', outputCurrency)
 searchParams.set('amount', amount.toString())
@@ -57,7 +60,7 @@ const { result } = await publicClient.simulateContract({
   account: to,
   value: json.routeProcessorArgs.value
 })
-console.log(result)
+console.log('Output: ', result)
 
 // Make a call to the blockchain for the swap
 // const PRIVATE_KEY = process.env.PRIVATE_KEY as Hex
